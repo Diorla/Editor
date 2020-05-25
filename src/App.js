11e7d04404bdccb3119cf4978237932c3bd33893
Ade Adeola <adedotster@gmail.com>
2020-05-25 15:38:49 +0100

    Add Ctrl+S shortcut to save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+//@ts-check
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Browser from "./sections/Browser";
-//@ts-check
-export default (props) => {
+import TitleBar from "./sections/TitleBar";
+import jsonfile from 'jsonfile';
+const { globalShortcut } = require("electron").remote;
+
+globalShortcut.register("CommandOrControl+S", () => {
+  const { file, content } = store.getState();
+  jsonfile.writeFile(file, content);
+  console.log("file saved")
+});
+
+export default () => {
   return (
     <Provider store={store}>
-      <div>This is app</div>;
+      <TitleBar />
       <Browser />
     </Provider>
   );
