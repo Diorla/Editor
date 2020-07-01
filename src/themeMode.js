9ac5c09520dbe5b95675e6a27b6fa19ba51f2360
Ade Adeola <adedotster@gmail.com>
2020-07-01 21:43:33 +0100

    Update theme

--- src/themeMode.js
+++ src/themeMode.js
@@ -5,31 +5,6 @@ import { createMuiTheme } from "@material-ui/core";
 export const darkTheme = createMuiTheme({
   palette: {
     type: "dark",
-    primary: {
-      light: "#e3c9aa",
-      main: "#ddbc95",
-      dark: "#9a8368",
-      contrastText: "black",
-    },
-    secondary: {
-      light: "#00695f",
-      main: "#009688", // teal
-      dark: "#33ab9f",
-      contrastText: "white",
-    },
-  },
-  props: {
-    MuiTextField: {
-      style: {
-        margin: 2,
-      },
-    },
-  },
-});
-
-export const lightTheme = createMuiTheme({
-  palette: {
-    type: "light",
     secondary: {
       light: "#ec9aa",
       main: "#ddbc95",
@@ -41,6 +16,22 @@ export const lightTheme = createMuiTheme({
       main: "#009688",
       dark: "#33ab9f",
       contrastText: "white",
+    },
+  },
+});
+
+export const lightTheme = createMuiTheme({
+  palette: {
+    type: "light",
+    secondary: {
+      light: "#ec9aa",
+      main: "#ddbc95",
+      dark: "#9a8368",
+    },
+    primary: {
+      light: "#00695f",
+      main: "#009688",
+      dark: "#33ab9f",
     },
   },
 });
