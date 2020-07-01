5bcf48d99f4acf380276cd8c02eda19a2f37e86c
Ade Adeola <adedotster@gmail.com>
2020-07-01 22:56:57 +0100

    Enable deletion

--- src/sections/TitleBar.js
+++ src/sections/TitleBar.js
@@ -4,12 +4,18 @@ import { connect } from "react-redux";
 import { makeStyles, Link } from "@material-ui/core";
 import { AiOutlineDelete } from "react-icons/ai";
 import { GiMoon } from "react-icons/gi";
-import { IoMdHelp, IoMdSettings } from "react-icons/io";
-import { FaRegStickyNote } from "react-icons/fa";
-import { CHANGE_THEME } from "../redux/constant";
-import { CLOSE_PROJECT } from "./../redux/constant";
+// import { IoMdHelp, IoMdSettings } from "react-icons/io";
+// import { FaRegStickyNote } from "react-icons/fa";
+import {
+  CHANGE_THEME,
+  OPEN_COLLECTION,
+  OPEN_PROJECT,
+  CLOSE_PROJECT,
+} from "./../redux/constant";
 import path from "path";
-
+import fs from "fs";
+// TODO: Add modal for opening settings and help
+// TODO: Add generator opener.
 const useStyles = makeStyles((theme) => ({
   appBar: {
     display: "flex",
@@ -38,11 +44,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 /**
- * @param {{ project?: {projectName: string, collectionDir: string, itemDir: string, activeBlog: string}; changeTheme?: ()=> void; closeProject?: ()=> void; }} props
+ * @param {{ project?: {projectName: string, collectionDir: string, itemDir: string, activeBlog: string}; changeTheme?: ()=> void; closeProject?: ()=> void; openFolder?: (arg0: string)=> void; openProject?: (arg0: string)=> void;}} props
  */
 const TitleBar = (props) => {
   const classes = useStyles();
-  const { changeTheme, closeProject } = props;
+  const { changeTheme, closeProject, openFolder, openProject } = props;
   const { projectName, collectionDir, itemDir, activeBlog } = props.project;
   const title =
     activeBlog ||
@@ -63,13 +69,47 @@ const TitleBar = (props) => {
       <div className={classes.appSection}>{`${title} - Tome Editor`}</div>
       <div className={classes.iconBar}>
         <GiMoon title="Dark mode" onClick={() => changeTheme()} />
-        <FaRegStickyNote title="Add note" onClick={() => console.log("note")} />
+        {/*<FaRegStickyNote title="Add note" onClick={() => console.log("note")} />
         <IoMdHelp title="Get help" onClick={() => console.log("help")} />
         <IoMdSettings
           title="Get preferences"
           onClick={() => console.log("settings")}
+        />*/}
+        <AiOutlineDelete
+          title="Delete"
+          onClick={() => {
+            // const { projectName, collectionDir, itemDir, activeBlog } = props.project;
+            const target = "" || itemDir || collectionDir || projectName;
+            if (
+              target &&
+              confirm(`Are you sure you want to delete ${target}`)
+            ) {
+              if (itemDir) {
+                openFolder(collectionDir);
+                fs.unlink(itemDir, (err) => {
+                  if (err) console.log(err);
+                  else console.log("file deleted");
+                });
+              } else if (collectionDir) {
+                openProject(projectName);
+                fs.rmdir(collectionDir, { recursive: true }, (err) => {
+                  if (err) console.log(err);
+                  else console.log("file deleted");
+                });
+              } else {
+                closeProject();
+                fs.rmdir(
+                  `${process.cwd()}/projects/${projectName}`,
+                  { recursive: true },
+                  (err) => {
+                    if (err) console.log(err);
+                    else console.log("file deleted");
+                  }
+                );
+              }
+            } else console.log("Not deleting");
+          }}
         />
-        <AiOutlineDelete title="Delete" onClick={() => console.log("delete")} />
       </div>
     </div>
   );
@@ -82,9 +122,6 @@ const mapStateToProps = (state) => ({
   project: state.project,
 });
 
-/**
- * @param {(arg0: { type: string; }) => any} dispatch
- */
 const mapDispatchToProps = (dispatch) => ({
   changeTheme: () =>
     dispatch({
@@ -93,6 +130,22 @@ const mapDispatchToProps = (dispatch) => ({
   closeProject: () =>
     dispatch({
       type: CLOSE_PROJECT,
+    }),
+  /**
+   * @param {string} collectionDir
+   */
+  openFolder: (collectionDir) =>
+    dispatch({
+      type: OPEN_COLLECTION,
+      collectionDir,
+    }),
+  /**
+   * @param {string} projectName
+   */
+  openProject: (projectName) =>
+    dispatch({
+      type: OPEN_PROJECT,
+      projectName,
     }),
 });
 
