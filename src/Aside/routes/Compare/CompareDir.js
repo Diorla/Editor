4e7a4b38a18b9bd18c1eb20e2dbf4fed9936ea2b
Ade Adeola <adedotster@gmail.com>
2020-07-10 09:52:08 +0100

    Enable filter

--- src/Aside/routes/Compare/CompareDir.js
+++ src/Aside/routes/Compare/CompareDir.js
@@ -5,6 +5,8 @@ import FileItem from "../../../components/FileItem";
 import { openCompare } from "../../../redux/aside";
 import klaw from "klaw";
 import ErrorLog from "../../../components/ErrorLog";
+import { TextField } from "@material-ui/core";
+import useStyles from "../../../components/useStyles";
 
 /**
  * @param {{ aside: {dir: string}; openCompare: (payload: {file: string})=> void; }} props
@@ -12,31 +14,43 @@ import ErrorLog from "../../../components/ErrorLog";
 const Dir = (props) => {
   const { aside, openCompare } = props;
   const [dirList, setDirList] = useState([]);
+  const [filter, setFilter] = useState("");
+  const classes = useStyles();
   useEffect(() => {
     const items = [];
     klaw(aside.dir)
       .on("data", (item) => {
         if (item.path.includes("scrb")) items.push(item.path);
       })
-      .on("error", (err) =>ErrorLog(err))
+      .on("error", (err) => ErrorLog(err))
       .on("end", () => setDirList(items));
   }, []);
   return (
     <div>
       <h2>Compare</h2>
-      {dirList.map((item, idx) => (
-        <FileItem
-          key={idx}
-          name={item}
-          ext=".scrb"
-          onClick={() =>
-            openCompare({
-              file: item,
-            })
-          }
-          type="file"
-        />
-      ))}
+      <TextField
+        placeholder="filter"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        className={classes.fullWidth}
+      />
+      {dirList
+        .filter((item) =>
+          item.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+        )
+        .map((item, idx) => (
+          <FileItem
+            key={idx}
+            name={item}
+            ext=".scrb"
+            onClick={() =>
+              openCompare({
+                file: item,
+              })
+            }
+            type="file"
+          />
+        ))}
     </div>
   );
 };
