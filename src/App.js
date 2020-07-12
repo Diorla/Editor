331f8f8fcfa6c41fde0a46fab99a6ef36f7d35d9
Ade Adeola <adedotster@gmail.com>
2020-07-12 21:04:20 +0100

    Remove unused variables

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import store from "./redux/store";
 import Layout from "./Layout";
 
 export default () => {
-  const { isDarkMode } = store.getState();
   return (
     <Provider store={store}>
       <Layout />
