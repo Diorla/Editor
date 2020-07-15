80f46626217d4d5326a26e159402d8ef03b24c32
Ade Adeola <adedotster@gmail.com>
2020-07-15 09:17:53 +0100

    Add option to return an integer

--- src/utils/random/range.js
+++ src/utils/random/range.js
@@ -4,7 +4,10 @@
  * It returns a float between two ranges, with the minimum and maximum being inclusive
  * @param {number} start - The fist end of the sequence indicating where to start (or end)
  * @param {number} end - The other end of the sequence indicating where to end (or start)
+ * @param {any} integer? - Whether to return an integer or float, it is false by default
  */
-export default (start, end) => {
-  return Math.random() * (end - start) + start;
+export default (start, end, integer=false) => {
+  return integer
+    ? Math.round(Math.random() * (end - start) + start)
+    : Math.random() * (end - start) + start;
 };
